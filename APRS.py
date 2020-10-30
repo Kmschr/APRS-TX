d50275f60c9760c506f319d35254d0a5a9e38073
Kevin Schroeder <Kmschr@gmail.com>
2020-10-29 23:18:51 -0600

    GPS Update

--- APRS.py
+++ APRS.py
@@ -1,59 +1,143 @@
+######################################################################
+# CSU ROCKET TEAM 2020-2021
+# FLIGHT COMPUTER PROGRAM
+# ROCKET: Aries CoVId
+######################################################################
+
+import os
 import sys
 import time
-import threading
+import math
+import busio
+import signal
+import serial
+import yaspin
+import argparse
 import subprocess
+import adafruit_gps
+from yaspin import yaspin, kbi_safe_yaspin
 from datetime import datetime
-from multiprocessing import Process
+
+script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+def signal_handler(sig, frame):
+    print('exiting')
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
 
 ######################################################################
 # OPTIONS
 ######################################################################
 
-# HAM License callsign - MUST BE YOUR CALLSIGN
-# NOT TRANSMITTING WITH YOUR CALLSIGN IS VIOLATION OF FCC
-# AND YOU CAN BE FINED HEAVILY
-CALLSIGN = 'KE0SPF'
-APRS_COMMENT = 'CSU ROCKET TEAM TESTING '
+parser = argparse.ArgumentParser(description='Transmit APRS information.')
+parser.add_argument('callsign', type=str,
+                    help='Your FCC HAM license callsign')
+parser.add_argument('--with-gps', dest='gps', action='store_true',
+                    help='The Adafruit GPS is hooked up')
+parser.add_argument('--without-gps', dest='gps', action='store_false',
+                    help='No GPS')
+parser.set_defaults(gps=True)
+args = parser.parse_args()
+
+CALLSIGN = args.callsign
+APRS_COMMENT = 'CSU ROCKET TEAM GPS TESTING'
 APRS_SYMBOL_ROCKET = 'O'
-TOTAL_TRANSMISSIONS = 3
+TRANSMIT_TIME_SECONDS = 65
+
+print('Callsign: ' + CALLSIGN)
+print('APRS_COMMENT: ' + APRS_COMMENT)
+print('Transmit Timer: ' + str(TRANSMIT_TIME_SECONDS) + 's')
+
+######################################################################
+# SETUP SENSORS
+######################################################################
+
+gps = None
+# Adafruit Ultimate GPS v3
+if args.gps:
+    uart = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=10)
+    gps = adafruit_gps.GPS(uart, debug=False)
+    gps.send_command(b'PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0')
+    gps.send_command(b'PMTK220,500')
+    gps.update()
+
+    with yaspin(text="Waiting for GPS") as sp:
+        while not gps.has_fix:
+            gps.update()
+            time.sleep(0.5)
 
 ######################################################################
 # TRANSMIT LOOP
 ######################################################################
 
 num_transmissions = 0
-transmit_repeat = 20e9
-play_time = time.time_ns() + transmit_repeat
+transmit_repeat = TRANSMIT_TIME_SECONDS * 1e9
+play_time = time.time_ns() + 2e9
 start_time = play_time
+
+color = ("red", "white", "green", "blue")
+spinner = kbi_safe_yaspin(text="")
+spinner.start()
 
 while True:
     # APRS Timestamp in format of Day/Hour/Minutes zulu time
     zulutime = datetime.utcnow().strftime("%d%H%M")
+
     # latitude in format ddmm.hhN (i.e degrees, minutes, and hundredths of a minute north)
-    lat = "4033.14N"
     # longitude in format dddmm.hhW (i.e degrees, minutes, and hundreths of a minute west)
-    lon = "10505.69W"
+    # indeterminate position signaled by 0000.00N\00000.00W
     # altitude in format aaaaaa where altitude is in feet
-    alt = "005003"
+    latitude = "0000.00N"
+    longitude = "00000.00W"
+    altitude = "005003"
+
+    if gps is not None and gps.update():
+        (latitude_min, latitude_deg) = math.modf(gps.latitude)
+        (longitude_min, longitude_deg) = math.modf(gps.longitude)
+        latitude_dir = 'N' if latitude_deg >= 0 else 'S'
+        longitude_dir = 'W' if longitude_deg <= 0 else 'E'
+        latitude_deg = '{:02d}'.format(abs(int(latitude_deg)))
+        longitude_deg = '{:03d}'.format(abs(int(longitude_deg)))
+        latitude_min = '{:05.2f}'.format(abs(round(latitude_min*60, 2)))
+        longitude_min = '{:05.2f}'.format(abs(round(longitude_min*60, 2)))
+        latitude = latitude_deg + latitude_min + latitude_dir
+        longitude = longitude_deg + longitude_min + longitude_dir
+        if gps.altitude_m is not None:
+            altitude = '{:06d}'.format(int(gps.altitude_m*3.28084))
+
     # time and position format
-    aprs_info = "/{}z{}\\{}{}{} /A={}".format(zulutime, lat, lon, APRS_SYMBOL_ROCKET, APRS_COMMENT + str(num_transmissions), alt)
+    aprs_info = '/{}z{}\\{}{}{} /A={}'.format(zulutime, 
+                                              latitude, 
+                                              longitude, 
+                                              APRS_SYMBOL_ROCKET, 
+                                              APRS_COMMENT, 
+                                              altitude)
+
+    spinner.text = CALLSIGN + '>APRS,WIDE1-1,WIDE2-1:' + aprs_info
+    spinner.color = color[num_transmissions % 4]
 
     # write APRS message as wave audio file
-    subprocess.run(["/home/pi/APRS-TX/afsk/aprs", "-c", CALLSIGN, "-o", "/home/pi/APRS-TX/packet"+str(num_transmissions)+".wav", aprs_info])
+    subprocess.run([script_path + "/afsk/aprs", 
+                    "-c", CALLSIGN, "-o", 
+                    "/tmp/packet"+str(num_transmissions%3)+".wav", 
+                    aprs_info])
     aprs_end = time.time_ns()
 
-    # make sure its been 2 seconds exactly before playing audio
+    # make sure its been required seconds exactly before playing audio
     wait_time = (play_time - time.time_ns()) / 1e9
     if wait_time > 0:
         time.sleep(wait_time)
     else:
         print("APRS message is too long")
-    print((time.time_ns() - start_time) / 1e9)
+    #print((time.time_ns() - start_time) / 1e9)
 
-    # play APRS message over default soundcard
-    proc = subprocess.Popen(["sudo aplay -q /home/pi/APRS-TX/packet"+str(num_transmissions)+".wav"], shell=True, stdin=None, stdout=None, stderr=None, close_fds=True, start_new_session=True)
+    # play APRS message over default soundcard in new non-blocking process
+    subprocess.Popen(["sudo aplay -q /tmp/packet"+str(num_transmissions%3)+".wav"], 
+                     shell=True, 
+                     stdin=None, 
+                     stdout=None, 
+                     stderr=None, 
+                     close_fds=True, 
+                     start_new_session=True)
     play_time += transmit_repeat
 
     num_transmissions += 1
-
-print((time.time_ns() - start_time) / 1e9)
--- README.md
+++ README.md
@@ -2,9 +2,8 @@
 APRS transmission from a Raspberry Pi interfaced w/ HT over standard audio output
 
 ## Installation and Setup
-Install afsk tool
 
-`$ pip install afsk`
+[GPS Wiring (Pi's UART)](https://learn.adafruit.com/adafruit-ultimate-gps/circuitpython-parsing)
 
 Clone this repository and go into it
 
@@ -12,6 +11,29 @@ Clone this repository and go into it
 
 `$ cd APRS-TX`
 
-**EXTREMELY IMPORTANT** Ensure correct callsign set in `APRS.py`
+Install dependencies
+
+`$ pip3 install -r requirements.txt`
 
 Ensure pi output is set to analog/headphones (NOT HDMI)
+
+`$ alsamixer`
+
+### Running normally
+
+`$ python3 APRS.py YOUR_CALLSIGN`
+
+### Running without the GPS attached
+
+`$ python3 APRS.py YOUR_CALLSIGN --without-gps`
+
+## Example cronjob
+
+For automatic startup when pi is turned on
+
+`$ crontab -e`
+
+```
+XDG_RUNTIME_DIR=/run/user/1000
+@reboot python3 /home/pi/APRS-TX/APRS.py KE0SPF &
+```
